28f80a702e6fbcd9e00797200ae1fbe8b4606091
geoffrey woodcock <theatomproject@gmail.com>
2018-01-13 18:29:40 +0000

    automatically backed up by learn

--- beyoncesAccountant.js
+++ beyoncesAccountant.js
@@ -4,10 +4,10 @@ console.log("You have earned " + beyoncesMoney)
 console.log("Of" + 500 + 750 + 3000 + 5100 + 12980 + 9087 + 50000 + " some portion derives from your live performances.")
 console.log("Of" + 500 + 750 + 3000 + 5100 + 12980 + 9087 + 50000 + " some portion derives from music sales.")
 
+var sum = 500 + 750 + 3000 + 5100 + 12980 + 9087 + 50000
+console.log(sum)
 
 // DON'T DO THIS PART YET:
 // Now that we understand what a variable is
 // rewrite the above lines of code using a variable to store the
 // total sum, so that you don't have to add up all those numbers three times
-
-
